ce9a3c1fe47fbb071b6ed77861477678815e3125
badr <badrelhoudni12@gmail.com>
2023-03-22 17:22:48 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - main function
+ * _putchar: print the alphabet putchar 
+ * Return: always 0
+ */
+
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for(ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+	return (0);
+}
