c2d8629624dea895088864b3d685e5c1d12761a2
badr <badrelhoudni12@gmail.com>
2023-04-29 00:25:10 +0100

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,30 +1,38 @@
+#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-#include "lists.h"
 
 /**
-  * add_node - adds a new node at the beginning of a list
-  * @head: the original linked list
-  * @str: the string to add to the node
-  *
-  * Return: the address of the new list or NULL if it failed
-  */
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new;
-	unsigned int len = 0;
+ * add_node - adds a node to the beginning of a linked list
+ * _strlen - finds the length of  strings
+ * @str: find the length of that string
+ *
+ * Return: It returns the length of that string
+ */
+typedef struct list_t {
+    char *str;
+    unsigned int len;
+    struct list_t *next;
+} list_t;
 
-	while (str[len])
-	len++;
-
-	new = malloc(sizeof(list_t));
-	if (!new)
-	return (NULL);
-
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
-
-	return (*head);
+unsigned int strlen(const char *str) {
+    unsigned int len = 0;
+    while (*str++) len++;
+    return len;
 }
+
+list_t *add_node(list_t **head, const char *str) {
+    if (!str) return NULL;
+    list_t *new_list_t = (list_t *) malloc(sizeof(list_t));
+    if (!new_list_t) return NULL;
+    new_list_t->str = strdup(str);
+    if (!new_list_t->str) {
+        free(new_list_t);
+        return NULL;
+    }
+    new_list_t->len = strlen(str);
+    new_list_t->next = *head;
+    *head = new_list_t;
+    return new_list_t;
+}
+
