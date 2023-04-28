7935ca10fd84e3f584975aa442dffc232c00efec
badr <badrelhoudni12@gmail.com>
2023-04-28 22:59:43 +0100

    List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+  * list_len - Show the number of elements of a list
+  * @h:linked list
+  * Return: The number of elements of a list
+  */
 size_t list_len(const list_t *h)
 {
 	size_t sf = 0;
