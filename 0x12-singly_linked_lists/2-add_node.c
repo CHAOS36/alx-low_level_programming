2e2cb97024822101b34173a413ec827b179c77f9
badr <badrelhoudni12@gmail.com>
2023-04-29 01:33:55 +0100

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,44 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * add_node - always adds a node to the beginning of a linked list
- * _strlen - finds the length of  strings
- * @str: find the length of that string
- * Return: It returns the length of that string
- */
-unsigned int _strlen(char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	unsigned int i;
+	if (head == NULL || str == NULL)
+		return (NULL);
 
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
+	list_t *new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
-/**
- * @head: double pointer to a lined list
- * Return: returns a pointer to the new node
- */
-list_t *add_node(list_t **list, const char *str_val)
-{
-    list_t *new_node;
 
-    if (str_val == NULL)
-        return (NULL);
-
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    new_node->str = strdup(str_val);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-
-    new_node->len = _str_len(new_node->str);
-    new_node->next = *list;
-    *list = new_node;
-    return (new_node);
-}
