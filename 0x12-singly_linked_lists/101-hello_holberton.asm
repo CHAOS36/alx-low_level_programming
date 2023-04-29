5fc54b3422a07fff3bcf7c4c536603af4726dbe9
badr <badrelhoudni12@gmail.com>
2023-04-29 02:16:03 +0100

    assembler

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+section .data
+    format db 'Hello, Holberton', 10, 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    push format
+    call printf
+    add  esp, 4
+    xor  eax, eax
+    ret
+
