539b8b67442cf59ed186cc3bbbe2985b4e2cfc00
badr <badrelhoudni12@gmail.com>
2023-04-29 23:54:04 +0100

    add node int

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,7 +4,7 @@
 /**
  * add_nodeint - always adds a new node at the beginning of a linked list
  * @head: double pointer to the first node of the linked list
- * @n:  integer value that represents the value to be stored in the new node that is being added to the linked list
+ * @n: int value that represents the value to be stored in the new node that is being added to the linked list
  *
  * Return: pointer to the new node, or NULL if it fails
  */
