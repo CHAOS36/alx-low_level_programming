0c9cd890756dd733433a4905764a2914da73df65
badr <badrelhoudni12@gmail.com>
2023-07-06 20:37:09 +0100

    bit manip

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,25 @@
 #include "main.h"
+
+/**
+ * binary_to_unit - converts a binary number to unsigned int
+ * @p: string containig the binary numbers
+ *
+ * Return: the converted numbers
+ */
+
+unsigned int binary_to_uint(const char *p)
+{
+	int k;
+	unsigned int yeye = 0;
+
+	if (!p)
+		return (0);
+	for (k = 0; p[k]; p++)
+	{
+		if(b[p] < '0' || b[p] > '1')
+			return (0);
+		yeye = 2 * yeye +(b[p] - '0');
+	}
+
+	return (yeye);
+}
