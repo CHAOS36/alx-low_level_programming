7df1257b35ed188e34e7a6483893070422f53802
badr <badrelhoudni12@gmail.com>
2023-07-07 15:17:58 +0100

    bit manip5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bits to change
+ * to get from one number to another
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to change
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int exclusive = n ^ m;
+	unsigned int count = __builtin_popcountl(exclusive);
+	return count;
+}
