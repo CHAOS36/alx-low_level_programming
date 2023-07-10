4c737a46f03de743dd549aca5278c71d1da06865
badr <badrelhoudni12@gmail.com>
2023-07-10 13:02:50 +0100

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	coma = open(filename, O_WRONLY | O_APPEND);
 	fr = write(coma, text_content, len);
 
-	if (coma == -1 || w == -1)
+	if (coma == -1 || fr == -1)
 		return (-1);
 
 	close(coma);
