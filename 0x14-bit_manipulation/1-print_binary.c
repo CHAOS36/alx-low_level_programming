59f10fffed1e14073e82497fdd63204a6d4fc708
badr <badrelhoudni12@gmail.com>
2023-11-24 02:47:59 +0100

    .

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,22 +8,22 @@
  */
 void print_binary(unsigned long int n)
 {
-        int bite = sizeof(n) * 8, kinter = 0;
+	int bite = sizeof(n) * 8, kinter = 0;
 
-        while (bite)
-        {
-                if (n & 1L << --bite)
-                {
-                        putchar('1');
-                        kinter++;
-                }
-                else if (kinter)
-                {
-                        putchar('0');
-                }
-        }
-        if (!kinter)
-        {
-                putchar('0');
-        }
+	while (bite)
+	{
+		if (n & 1L << --bite)
+		{
+			putchar('1');
+			kinter++;
+		}
+		else if (kinter)
+		{
+			putchar('0');
+		}
+	}
+	if (!kinter)
+	{
+		putchar('0');
+	}
 }
