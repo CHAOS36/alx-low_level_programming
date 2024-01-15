aeb521d05dd0fedd0517ac189e55c1b2340f26d8
CHAOS36 <badrelhoudni96@gmail.com>
2024-01-15 11:35:45 -0800

    .

--- 0x18-dynamic_libraries/putshack.c
+++ 0x18-dynamic_libraries/putshack.c
@@ -1,22 +1,25 @@
 #include <unistd.h>
 #include <string.h>
+/**
+ * descriptions for rand
+ */
 
-int rand()
+int rand(void)
 {
 	static int pipo = -1;
 
 	pipo++;
 	if (pipo == 0)
-		return 8;
+		return (8);
 	if (pipo == 1)
-		return 8;
+		return (8);
 	if (pipo == 2)
-		return 7;
+		return (7);
 	if (pipo == 3)
-		return 9;
+		return (9);
 	if (pipo == 4)
-		return 23;
+		return (23);
 	if (pipo == 5)
-		return 74;
-	return pipo * pipo % 30000;
+		return (74);
+	return (pipo * pipo % 30000);
 }
