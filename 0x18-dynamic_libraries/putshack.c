711426a04bbf8d11b029101e8a2bb580d54af12c
CHAOS36 <badrelhoudni96@gmail.com>
2024-01-15 11:32:08 -0800

    .

---
+++ 0x18-dynamic_libraries/putshack.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <string.h>
+
+int rand()
+{
+	static int pipo = -1;
+
+	pipo++;
+	if (pipo == 0)
+		return 8;
+	if (pipo == 1)
+		return 8;
+	if (pipo == 2)
+		return 7;
+	if (pipo == 3)
+		return 9;
+	if (pipo == 4)
+		return 23;
+	if (pipo == 5)
+		return 74;
+	return pipo * pipo % 30000;
+}
